b801dce96b5e947e011add1f5b88bf28f69a022c
tremblayalex <47582129+tremblayalex@users.noreply.github.com>
2020-02-05 12:30:11 -0500

    Modification de la classe 'Joeur' pour y ajouter des fonctionnalités de base

--- travail1/joueur.cpp
+++ travail1/joueur.cpp
@@ -1,2 +1,45 @@
 #include "joueur.h"
 
+Joueur::Joueur() :
+	nom(""),
+	nombreVictoires(0),
+	nombreDefaites(0),
+	indexDerniereCarteAJouer(0),
+	mainDuJoueur()
+{}
+
+void Joueur::setNomJoueur(const string& inNom)
+{
+	nom = inNom;
+}
+
+void Joueur::ajouterUneVictoire()
+{
+	nombreVictoires++;
+}
+
+void Joueur::ajouterUneDefaite()
+{
+	nombreDefaites++;
+}
+
+void Joueur::ajouterUneCarte(Carte* inCarte)
+{
+	if (indexDerniereCarteAJouer < maxCartesAJouer)
+	{
+		mainDuJoueur[indexDerniereCarteAJouer] = inCarte;
+
+		indexDerniereCarteAJouer++;
+	}
+	else
+	{
+		cout << "[Erreur] Espace maximal excédé pour le tableau de carte 'manDuJoueur'" << endl;
+		cout << "À la ligne" << __LINE__ << endl;
+	}
+	
+}
+
+void Joueur::enleverToutesLesCartes()
+{
+	indexDerniereCarteAJouer = 0;
+}
--- travail1/joueur.h
+++ travail1/joueur.h
@@ -6,8 +6,24 @@ using namespace std;
 #define JoueurH
 
 const int maxCartesAJouer = 26;
+
 class Joueur
 {
+private:
+	string nom;
+	int nombreVictoires;
+	int nombreDefaites;
 
+	int indexDerniereCarteAJouer;
+	Carte* mainDuJoueur[maxCartesAJouer];
+
+public:
+	Joueur();
+
+	void setNomJoueur(const string& inNom);
+	void ajouterUneVictoire();
+	void ajouterUneDefaite();
+	void ajouterUneCarte(Carte* inCarte);
+	void enleverToutesLesCartes();
 };
 #endif
